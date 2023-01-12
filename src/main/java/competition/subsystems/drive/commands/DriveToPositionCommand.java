e7748446ff37f1cbdeed1958a240b3dc043bb918
unknown <sw7859305@gmail.com>
2023-01-12 09:25:26 -0800

    using PID manager (old ver)

--- src/main/java/competition/subsystems/drive/commands/DriveToPositionCommand.java
+++ src/main/java/competition/subsystems/drive/commands/DriveToPositionCommand.java
@@ -5,7 +5,8 @@ package competition.subsystems.drive.commands;
 import com.google.inject.Inject;
 
 import xbot.common.command.BaseCommand;
-
+import xbot.common.injection.wpi_factories.CommonLibFactory;
+import xbot.common.math.PIDManager;
 import competition.subsystems.drive.DriveSubsystem;
 import competition.subsystems.pose.PoseSubsystem;
 
@@ -14,15 +15,25 @@ public class DriveToPositionCommand extends BaseCommand {
 
     DriveSubsystem drive;
     PoseSubsystem pose;
+    PIDManager pid;
     public double goalPosition;
     public double prePosition;
    
     
 
     @Inject
-    public DriveToPositionCommand(DriveSubsystem driveSubsystem, PoseSubsystem pose) {
+    public DriveToPositionCommand(DriveSubsystem driveSubsystem, PoseSubsystem pose, CommonLibFactory clf) {
         this.drive = driveSubsystem;
         this.pose = pose;
+        this.pid = clf.createPIDManager("DriveToPoint");
+        pid.setEnableErrorThreshold(true); // turn on distance checking
+        pid.setErrorThreshold(0.1);
+        pid.setEnableDerivativeThreshold(true); // turn on speed checking
+        pid.setDerivativeThreshold(0.1);
+
+        // manually adjust values
+        pid.setP(2);
+        pid.setD(3);
     }
 
     public void setTargetPosition(double position) {
@@ -36,6 +47,7 @@ public class DriveToPositionCommand extends BaseCommand {
     @Override
     public void initialize() {
         // If you have some one-time setup, do it here.
+        pid.reset();
     }
 
     @Override
@@ -50,7 +62,8 @@ public class DriveToPositionCommand extends BaseCommand {
         //total distance
         double denominator = goalPosition;
         //sets power to slow down at target position
-        double power = (goal/denominator) * 2 - (pose.getPosition() - prePosition) * 3;
+        // double power = (goal/denominator) * 2 - (pose.getPosition() - prePosition) * 3;
+        double power = pid.calculate(goal, denominator);
         log.info(power);
         drive.tankDrive(power,power);
         
@@ -69,11 +82,7 @@ public class DriveToPositionCommand extends BaseCommand {
         // Modify this to return true once you have met your goal,
         // and you're moving fairly slowly (ideally stopped)
     
-        if(goalPosition < pose.getPosition()){
-           
-            return true;
-        }
-        return false;
+        return pid.isOnTarget();
     }
 
 }
